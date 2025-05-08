5f2589c21321a7c54fbaed10dc89c68dc8a4aa8c
gabor <karancsgabor@gmail.com>
2025-05-08 15:14:24 +0200

    POM

--- Sprint_1/POM/Purchase/cart_page.py
+++ Sprint_1/POM/Purchase/cart_page.py
@@ -1,31 +1,20 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-
 from Sprint_1.POM.GeneralPage import GeneralPage
 
-class MyCartPage(GeneralPage):
+
+class MyCartPageClass(GeneralPage):
     def __init__(self, browser):
         self.url = 'http://localhost:4200/registration'
         super().__init__(self.url, browser)
-        self.wait = WebDriverWait(self.browser, 5)
 
     def open_cart(self):
-        self.wait(By.ID, "button_myCart").click()
+        self.webelement_by_id("button_myCart").click()
 
     def continue_shopping(self):
-        self.browser.find_element(By.XPATH, "//span[text()='Continue shopping']").click()
+        self.webelement_by_xpath("//span[text()='Continue shopping']").click()
 
-    def remove_item(self, index=0):
-        try:
-            minus_buttons = self.browser.find_elements(By.XPATH, "//button[contains(@class, 'minus_button')]")
-            if minus_buttons:
-                minus_buttons[index].click()
-
-            x_buttons = self.browser.find_elements(By.XPATH, "//mat-icon[text()='close']")
-            if x_buttons:
-                x_buttons[index].click()
-        except IndexError:
-            print("Item not found at the given index.")
+    def remove_item(self):
+        minus_buttons = self.webelement_by_xpath("//button[contains(@class, 'minus_button')]")
+        x_buttons = self.webelement_by_xpath("//mat-icon[text()='close']")
 
     def click_checkout(self):
-        self.browser.find_element(By.XPATH, "//span[text()='Check out']").click()
+        self.webelement_by_xpath("//span[text()='Check out']").click()
--- Sprint_1/POM/Purchase/purchase_page.py
+++ Sprint_1/POM/Purchase/purchase_page.py
@@ -1,61 +1,61 @@
-from selenium.webdriver.common.by import By
 from Sprint_1.POM.GeneralPage import GeneralPage
 
-class PurchasePage(GeneralPage):
+
+class PurchasePageClass(GeneralPage):
     def __init__(self, browser):
         self.url = 'http://localhost:4200/registration'
         super().__init__(self.url, browser)
 
     def products_list(self):
-        return (self.webelement_by_xpath('//div[contains(@class, "col") and contains(@class, "ng-star-inserted") and @id]')
-            self.browser.find_elements(By.XPATH, '//div[contains(@class, "col") and contains(@class, "ng-star-inserted") and @id]'))
+        return self.webelement_by_xpath(
+            '//div[contains(@class, "col") and contains(@class, "ng-star-inserted") and @id]')
 
     def product_add_to_cart(self, index=0):
-        self.browser.find_elements(By.ID, 'button_addToCart')[index].click()
+        self.webelement_by_id('button_addToCart')[index].click()
 
     def open_cart(self):
-        self.browser.find_element(By.ID, "button_myCart").click()
+        self.webelement_by_id("button_myCart").click()
 
     def click_checkout(self):
-        self.browser.find_element(By.XPATH, "//span[text()='Check out']").click()
+        self.webelement_by_xpath("//span[text()='Check out']").click()
 
     def back_to_cart(self):
-        self.browser.find_element(By.XPATH, "//span[text()='Back to the cart ']").click()
+        self.webelement_by_xpath("//span[text()='Back to the cart ']").click()
 
     def click_next(self):
-        self.browser.find_element(By.CLASS_NAME, "next_btn").click()
+        self.webelements_by_classname("next_btn").click()
 
     def enter_customer_details(self, name, email, phone):
-        self.browser.find_element(By.XPATH, '//input[@id="formName_input"]').send_keys(name)
-        self.browser.find_element(By.XPATH, '//input[@id="formEmail_input"]').send_keys(email)
-        self.browser.find_element(By.XPATH, '//input[@id="formPhoneNumber_input"]').send_keys(phone)
+        self.webelement_by_xpath('//input[@id="formName_input"]').send_keys(name)
+        self.webelement_by_xpath('//input[@id="formEmail_input"]').send_keys(email)
+        self.webelement_by_xpath('//input[@id="formPhoneNumber_input"]').send_keys(phone)
 
     def billing_details(self, name, zip_code, city, street_nr):
-        self.browser.find_element(By.XPATH, '//input[@id="formName1_input"]').send_keys(name)
-        self.browser.find_element(By.XPATH, '//input[@id="formZIP_input"]').send_keys(zip_code)
-        self.browser.find_element(By.XPATH, '//input[@id="city_input"]').send_keys(city)
-        self.browser.find_element(By.XPATH, '//input[@id="adress_input"]').send_keys(street_nr)
+        self.webelement_by_xpath('//input[@id="formName1_input"]').send_keys(name)
+        self.webelement_by_xpath('//input[@id="formZIP_input"]').send_keys(zip_code)
+        self.webelement_by_xpath('//input[@id="city_input"]').send_keys(city)
+        self.webelement_by_xpath('//input[@id="adress_input"]').send_keys(street_nr)
         self.click_next()
 
     def shipping_details(self, name, zip_code, city, street_nr):
-        self.browser.find_element(By.XPATH, '//input[@id="formName1_input"]').send_keys(name)
-        self.browser.find_element(By.XPATH, '//input[@id="formZIP_input"]').send_keys(zip_code)
-        self.browser.find_element(By.XPATH, '//input[@id="city_input"]').send_keys(city)
-        self.browser.find_element(By.XPATH, '//input[@id="adress_input"]').send_keys(street_nr)
+        self.webelement_by_xpath('//input[@id="formName1_input"]').send_keys(name)
+        self.webelement_by_xpath('//input[@id="formZIP_input"]').send_keys(zip_code)
+        self.webelement_by_xpath('//input[@id="city_input"]').send_keys(city)
+        self.webelement_by_xpath('//input[@id="adress_input"]').send_keys(street_nr)
         self.click_next()
 
     def delivery_info(self, note="Ring the Bell"):
-        text_area = self.browser.find_element(By.ID, 'delivery_input').send_keys(note)
+        text_area = self.webelement_by_id('delivery_input').send_keys(note)
         self.click_next()
 
     def payment_opt(self, method="card"):
         if method == "cash":
-            self.browser.find_element(By.ID, 'mat-radio-14').click()
+            self.webelement_by_id('mat-radio-14').click()
         else:
-            self.browser.find_element(By.ID, 'mat-radio-15').click()
-        self.browser.find_element(By.CLASS_NAME, "send_btn").click()
+            self.webelement_by_id('mat-radio-15').click()
+        self.webelement_by_classname("send_btn").click()
 
     def get_payment_confirmation_message(self):
-        actual_message = self.browser.find_element(By.XPATH, "//p[contains(text(), 'The order has been sent')]").text
+        actual_message = self.webelement_by_xpath("//p[contains(text(), 'The order has been sent')]").text
         assert "The order has been sent" in actual_message, f"Expected message 'The order has been sent', but got: {actual_message}"
         return actual_message
