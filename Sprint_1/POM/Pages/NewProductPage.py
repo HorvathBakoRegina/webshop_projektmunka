16be260fcd1015b87f6d16fc5ef605539a11fe88
Csete Orsolya <cseteorsolya@gmail.com>
2025-05-16 14:21:40 +0200

    corrections

---
+++ Sprint_1/POM/Pages/NewProductPage.py
@@ -0,0 +1,34 @@
+from Sprint_1.POM.Pages.GeneralPage import GeneralPageClass
+
+
+class NewProductPageClass(GeneralPageClass):
+    def __init__(self, browser):
+        self.url = 'http://localhost:4200/new-product'
+        super().__init__(self.url, browser)
+
+    def product_name(self):
+        return self.webelement_by_id('input_pName')
+
+    def product_category(self):
+        return self.webelement_by_id('input_Select')
+
+    def product_categories(self, index):
+        return self.webelement_by_xpath(f'//mat-option[@id="input_option"][{index}]')
+
+    def product_price(self):
+        return self.webelement_by_id('input_pPrice')
+
+    def product_description(self):
+        return self.webelement_by_id('input_pDesc')
+
+    def choose_an_image(self):
+        return self.webelement_by_classname('upload-btn')
+
+    def save_product(self):
+        return self.webelement_by_id('button_saveProduct')
+
+    def cancel_button(self):
+        return self.webelement_by_id('button_cancel')
+
+    def success_message(self):
+        return self.webelement_by_xpath(f'//mat-option[@id="input_option"][{index}]')
